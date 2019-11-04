16b3c7fbf7258bf4bcadf8df912054196f911bc1
David Searl <dhsearl@gmail.com>
2019-11-03 22:45:21 -0600

    fixed missing import on pageThree.js

--- src/components/PageThree/PageThree.js
+++ src/components/PageThree/PageThree.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import InputRange from 'react-input-range';
 import 'react-input-range/lib/css/index.css'
+import { Box } from '@material-ui/core'
 
 
 class PageThree extends Component {
